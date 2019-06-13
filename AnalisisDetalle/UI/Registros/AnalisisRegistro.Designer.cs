d2a68bb9f05be74a516b274f9366ffc1251f5a15
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-13 10:54:26 -0400

    Configurando el connection string

--- AnalisisDetalle/UI/Registros/AnalisisRegistro.Designer.cs
+++ AnalisisDetalle/UI/Registros/AnalisisRegistro.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(360, 286);
+            this.ClientSize = new System.Drawing.Size(386, 343);
             this.Name = "AnalisisRegistro";
             this.Text = "Analisis";
             this.ResumeLayout(false);
