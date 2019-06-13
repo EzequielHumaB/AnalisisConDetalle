cac3ac43e84a0f5fc2fedd11ba9a4af2e95691ec
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-13 11:09:08 -0400

    Creando la entidad usuario

--- AnalisisDetalle/Entidades/Analisis.cs
+++ AnalisisDetalle/Entidades/Analisis.cs
@@ -8,6 +8,16 @@ namespace AnalisisDetalle.Entidades
 {
    public class Analisis
     {
+        public int AnalisisId { get; set; }
+        public int TipoId { get; set; }
+        public string resultado { get; set; }
+        
+        public Analisis()
+        {
+            AnalisisId = 0;
+            TipoId = 0;
+            resultado = string.Empty;
+        }
 
     }
 }
---
+++ AnalisisDetalle/Entidades/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisDetalle.Entidades
+{
+   public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string nombre { get; set; }
+        public string email { get; set; }
+        public string usuario { get; set; }
+        public DateTime fechaIngreso { get; set; }
+       
+        public Usuario()
+        {
+            UsuarioId = 0;
+            nombre = string.Empty;
+            email = string.Empty;
+            usuario = string.Empty;
+            fechaIngreso = DateTime.Now;
+        }
+
+    }
+}
